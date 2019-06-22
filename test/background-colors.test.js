5b2bc0d3cac954d523442b23e9cf3ee229293177
Patrick Cate <git@patrickcate.com>
2019-06-22 17:33:27 -0400

    feat: add support for transparent and currentColor color keywords

--- test/background-colors.test.js
+++ test/background-colors.test.js
@@ -1,13 +1,38 @@
-"use strict"
-
 const { getTestOptions, assertEqual } = require('./test-utils');
 
 /* eslint-disable func-names */
 describe('Background Colors', function() {
+  it('should extract the `transparent` color keyword to a separate `background-color` declaration.', function(done) {
+    assertEqual(
+      'a { background: transparent url("/images/arrow-next.png#elements"); }',
+      'a { background-color: transparent; background-image: url(sprites/elements.png); background-position: 0 0; }',
+      getTestOptions(),
+      done
+    );
+  });
+
+  it('should extract the `currentColor` color keyword to a separate `background-color` declaration.', function(done) {
+    assertEqual(
+      'a { background: currentColor url("/images/arrow-next.png#elements"); }',
+      'a { background-color: currentColor; background-image: url(sprites/elements.png); background-position: 0 0; }',
+      getTestOptions(),
+      done
+    );
+  });
+
   it('should extract the hexadecimal color to a separate `background-color` declaration.', function(done) {
     assertEqual(
       'a { background: #000000 url("/images/arrow-next.png#elements"); }',
       'a { background-color: #000000; background-image: url(sprites/elements.png); background-position: 0 0; }',
+      getTestOptions(),
+      done
+    );
+  });
+
+  it('should extract the hexadecimal shorthand color to a separate `background-color` declaration.', function(done) {
+    assertEqual(
+      'a { background: #fff url("/images/arrow-next.png#elements"); }',
+      'a { background-color: #fff; background-image: url(sprites/elements.png); background-position: 0 0; }',
       getTestOptions(),
       done
     );
@@ -24,8 +49,8 @@ describe('Background Colors', function() {
 
   it('should extract the RGBa color to a separate `background-color` declaration.', function(done) {
     assertEqual(
-      'a { background: rgba(0, 0, 0, 1) url("/images/arrow-next.png#elements"); }',
-      'a { background-color: rgba(0, 0, 0, 1); background-image: url(sprites/elements.png); background-position: 0 0; }',
+      'a { background: rgba(0,0,0,1) url("/images/arrow-next.png#elements"); }',
+      'a { background-color: rgba(0,0,0,1); background-image: url(sprites/elements.png); background-position: 0 0; }',
       getTestOptions(),
       done
     );
@@ -42,8 +67,8 @@ describe('Background Colors', function() {
 
   it('should extract the HSLa color to a separate `background-color` declaration.', function(done) {
     assertEqual(
-      'a { background: hsla(0,100%, 50%, 1) url("/images/arrow-next.png#elements"); }',
-      'a { background-color: hsla(0,100%, 50%, 1); background-image: url(sprites/elements.png); background-position: 0 0; }',
+      'a { background: hsla(0, 100%, 50%,1) url("/images/arrow-next.png#elements"); }',
+      'a { background-color: hsla(0, 100%, 50%,1); background-image: url(sprites/elements.png); background-position: 0 0; }',
       getTestOptions(),
       done
     );
