61056a2db9c3dc7c09686c3cefb9ef86b857ca2b
Patrick Cate <git@patrickcate.com>
2019-08-09 23:46:33 -0400

    chore(npm): update npm dependencies and new linting rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const { task, src, dest, series } = require('gulp');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
-const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const del = require('del');
 const easysprite = require('./index.js');
@@ -30,15 +29,11 @@ task('runDemo', (done) => {
   done();
 });
 
-task('linting', () => {
-  return src('**/*.js').pipe(eslint()); // hint (optional)
-});
-
 task('runTest', () => {
   return src('test/*.test.js', { read: false }).pipe(mocha());
 });
 
 // task('project', series('project:basic'));
 task('demo', series('clean:demo', 'runDemo'));
-task('test', series('linting', 'runTest'));
+task('test', series('runTest'));
 task('default', series('test'));
